b7d6c0f23e61743d9f2ce118071db0f47fc50f3f
crash <crash@server.ru>
2022-04-21 07:11:37 +0000

    deploy

--- web/config/settings.py
+++ web/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ms)d%wub_w+ie(!%ei^ezf9q8hk=mpt(lmcwm#6@bfze@fu04w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.150.101']
 
 
 # Application definition
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
