f35f0af109a83d57d6ee0a49210cb2c8e75b5d4e
Aleksandr Sirotin <sirotin92@yandex.ru>
2022-04-25 14:14:56 +0300

    bug fix, font change, css

--- web/config/settings.py
+++ web/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ms)d%wub_w+ie(!%ei^ezf9q8hk=mpt(lmcwm#6@bfze@fu04w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.150.101']
+ALLOWED_HOSTS = ['192.168.150.101', '127.0.0.1']
 
 
 # Application definition
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+# STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
